93c09ad4fd4bc943380c80834aae91ce53fddf92
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2021-03-18 20:35:35 +0500

    Add google benchmark

---
+++ benchmarks/json_bench.cpp
@@ -0,0 +1,18 @@
+#include "jsonner.h"
+
+#include <benchmark/benchmark.h>
+#include <fstream>
+
+static void BM_SomeFunction(benchmark::State &state)
+{
+   std::ifstream jsonFile("test.json");
+
+   jsonner::parser parser(jsonFile);
+   // Perform setup here
+   for (auto _ : state)
+   {
+      parser.parse();
+   }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_SomeFunction);
